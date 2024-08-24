bbfcd5df3c0789c9a50ae27cd84e8ea528609a09
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-08-24 19:40:21 +0530

    Added Spring Security Functionality with single user

---
+++ 15_Spring_Security_With_Single_User/src/main/java/com/dhruv/SpringSecurity/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.dhruv.SpringSecurity.controller;
+
+import com.dhruv.SpringSecurity.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students=new ArrayList<>(List.of(
+            new Student(1,"Dhrupad","Java"),
+            new Student(2,"Dhruv","Python")
+    ));
+
+    // Generate CSRF Token
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+}
---
+++ 15_Spring_Security_With_Single_User/src/main/java/com/dhruv/SpringSecurity/model/Student.java
@@ -0,0 +1,15 @@
+package com.dhruv.SpringSecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+    private String tech;
+}
